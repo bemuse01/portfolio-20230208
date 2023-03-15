6dbea911df93213fa5fceed4da2d6af62912b6ba
bemuse01 <sequeland@gmail.com>
2023-03-15 17:54:16 +0900

    update

--- assets/js/data/data.js
+++ assets/js/data/data.js
@@ -21,97 +21,135 @@ export default {
         {
             type: 'three',
             thumbPath: 'three-3d-map.png',
-            url: 'https://bemuse01.github.io/test-3d-map/'
+            url: 'https://bemuse01.github.io/test-3d-map/',
+            title: '3D Country Map',
+            desc: 'Cyberpunk 느낌의 여러 국가 3D 맵'
         },
         {
             type: 'three',
             thumbPath: 'three-no-title-20220526.png',
-            url: 'https://bemuse01.github.io/no-title-20220526/'
+            url: 'https://bemuse01.github.io/no-title-20220526/',
+            title: 'DNA',
+            desc: 'DNA 및 비주얼 앱 2D 시뮬레이션'
         },
         {
             type: 'three',
             thumbPath: 'three-rain-20220729.png',
-            url: 'https://bemuse01.github.io/test-shader-v.20220729/'
+            url: 'https://bemuse01.github.io/test-shader-v.20220729/',
+            title: 'Rain1',
+            desc: '비가 오는 느낌을 낸 2D 그래픽'
         },
         {
             type: 'three',
             thumbPath: 'three-rain-20220916.png',
-            url: 'https://bemuse01.github.io/test-shader-v.20220916/'
+            url: 'https://bemuse01.github.io/test-shader-v.20220916/',
+            title: 'Rain2',
+            desc: `비가 오는 느낌을 낸 2D 그래픽 \n (1과 차이점은 빗방울의 경로)`
         },
         {
             type: 'three',
             thumbPath: 'three-shader-20210412.png',
-            url: 'https://bemuse01.github.io/test-shader/'
+            url: 'https://bemuse01.github.io/test-shader/',
+            title: 'Particles',
+            desc: '마우스 상호 작용이 가능한 2D 파티클'
         },
         {
             type: 'three',
             thumbPath: 'three-text-animation-20220318.png',
-            url: 'https://bemuse01.github.io/WE-project-v.20220318/'
+            url: 'https://bemuse01.github.io/WE-project-v.20220318/',
+            title: 'Text Effect',
+            desc: '파티클 이펙트가 사용된 2D 텍스트 그래픽'
         },
         {
             type: 'three',
             thumbPath: 'three-visualizer-20210323.png',
-            url: 'https://bemuse01.github.io/BanG_Dream/'
+            url: 'https://bemuse01.github.io/BanG_Dream/',
+            title: 'Audio Visualizer',
+            desc: 'CSS와 2D 그래픽이 합쳐진 오디오 시각화'
         },
         {
             type: 'three',
             thumbPath: 'three-visualizer-20210727.png',
-            url: 'https://bemuse01.github.io/audio-visualizer-v.20210727/'
+            url: 'https://bemuse01.github.io/audio-visualizer-v.20210727/',
+            title: 'Audio Visualizer',
+            desc: '아이폰 시리의 비주얼을 흉내낸 오디오 시각화'
         },
         {
             type: 'three',
             thumbPath: 'three-visualizer-20220308.png',
-            url: 'https://bemuse01.github.io/audio-visualizer-v.20220308/'
+            url: 'https://bemuse01.github.io/audio-visualizer-v.20220308/',
+            title: 'Audio Visualizer',
+            desc: '체적 빛 효과가 사용된 정육면체 모양의 3D 오디오 시각화'
         },
         {
             type: 'three',
             thumbPath: 'three-visualizer-20220325.png',
-            url: 'https://bemuse01.github.io/audio-visualizer-v.20220325/'
+            url: 'https://bemuse01.github.io/audio-visualizer-v.20220325/',
+            title: 'Audio Visualizer',
+            desc: '애프터 이펙트의 플러그인을 흉내낸 3D 오디오 시각화'
         },
         {
             type: 'three',
             thumbPath: 'three-visualizer-20221020.png',
-            url: 'https://bemuse01.github.io/audio-visualizer-v.20221020/'
+            url: 'https://bemuse01.github.io/audio-visualizer-v.20221020/',
+            title: 'Audio Visualizer',
+            desc: '약 2만 6천개의 파티클이 모여서 형성된 고리 모양의 2D 오디오 시각화'
         },
         {
             type: 'three',
             thumbPath: 'three-visualizer-20220418.png',
-            url: 'https://bemuse01.github.io/three-audio-visualizer-20220418/'
+            url: 'https://bemuse01.github.io/three-audio-visualizer-20220418/',
+            title: 'Audio Visualizer',
+            desc: '모션 블러와 글로우 효과가 사용된 2D 오디오 시각화'
         },
         {
             type: 'three',
             thumbPath: 'three-shader-20220105.png',
-            url: 'https://bemuse01.github.io/test-three-v.20220105/'
+            url: 'https://bemuse01.github.io/test-three-v.20220105/',
+            title: 'GPGPU Image Effect',
+            desc: '이미지를 파티클 조각으로 나눠서 하드웨어 가속으로 이펙트를 처리한 3D 그래픽'
         },
         {
             type: 'three',
             thumbPath: 'arknights-20220128.png',
-            url: 'https://bemuse01.github.io/Arknights-v.20220128/'
+            url: 'https://bemuse01.github.io/Arknights-v.20220128/',
+            title: 'Arknights',
+            desc: '모바일 게임의 로딩 화면과 대기 화면을 웹으로 구현한 2D, 3D 그래픽 웹 앱'
         },
         {
             type: 'three',
             thumbPath: 'deemo-x-hololive.png',
-            url: 'https://bemuse01.github.io/deemo-x-hololive/'
+            url: 'https://bemuse01.github.io/deemo-x-hololive/',
+            title: 'Audio Playlist',
+            desc: '모바일 게임의 UI를 흉내낸 오디오 플레이리스트 웹 앱'
         },
         {
             type: 'babylon',
             thumbPath: 'babylon-visualizer-20221205.png',
-            url: 'https://bemuse01.github.io/test-babylon-20221205/'
+            url: 'https://bemuse01.github.io/test-babylon-20221205/',
+            title: 'Audio Visualizer',
+            desc: '파티클이 모여서 형성된 고리 모양의 2D 오디오 시각화 (Babylon.js 사용)'
         },
         {
             type: 'babylon',
             thumbPath: 'babylon-visualizer-20221214.png',
-            url: 'https://bemuse01.github.io/babylon-audio-visualizer-20221214/'
+            url: 'https://bemuse01.github.io/babylon-audio-visualizer-20221214/',
+            title: 'Audio Visualizer',
+            desc: '체적 빛 효과가 사용된 2D 오디오 시각화'
         },
         {
             type: 'babylon',
             thumbPath: 'babylon-visualizer-20230109.png',
-            url: 'https://bemuse01.github.io/babylon-audio-visualizer-20230109/'
+            url: 'https://bemuse01.github.io/babylon-audio-visualizer-20230109/',
+            title: 'Audio Visualizer',
+            desc: '파티클 메시와 글로우 효과가 사용된 2D 오디오 시각화'
         },
         {
             type: 'babylon',
             thumbPath: 'babylon-visualizer-20230126.png',
-            url: 'https://bemuse01.github.io/babylon-audio-visualizer-20230126/'
+            url: 'https://bemuse01.github.io/babylon-audio-visualizer-20230126/',
+            title: 'Audio Visualizer',
+            desc: '체적 빛 효과가 사용된 정육면체 모양의 3D 오디오 시각화 (Babylon.js 사용)'
         },
     ]
 }
